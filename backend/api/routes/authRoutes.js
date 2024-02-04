7849e3a64ffd03fd857a29c0df8a8d25f152172d
Ravi Gelani <ravi.gelanee@gmail.com>
2024-02-04 11:19:41 -0500

    resolve issue

--- backend/api/routes/authRoutes.js
+++ backend/api/routes/authRoutes.js
@@ -14,7 +14,7 @@ router.post('/consultantLogin', authConsultantController.consultant_login);
 router.post('/consultantCreatepassword', authConsultantController.create_password_consultant);
 router.get('/consultantVerify/:token', authConsultantController.verify_email);
 router.delete('/deleteConsultant',authMiddleware.authenticateToken, authSuperController.delete_consultant_by_admin);
-router.delete('/deletecUser',authMiddleware.authenticateToken, authConsultantController.delete_user_by_consultant);
+router.delete('/deleteUser',authMiddleware.authenticateToken, authConsultantController.delete_user_by_consultant);
 
 module.exports = router;
 
