86bc24773f5f75da8722cfc74bfe4fd50c8181c0
Ravi Gelani <ravi.gelanee@gmail.com>
2024-02-15 21:25:09 -0500

    add user

--- frontend/src/_nav_consultant.js
+++ frontend/src/_nav_consultant.js
@@ -24,6 +24,32 @@ const _nav_consultant = [
       // text: 'NEW',
     },
   },
+
+  {
+    component: CNavGroup,
+    name: 'User',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Add User',
+        to: '/adduser',
+        icon: <CIcon icon={cilNoteAdd} customClassName="nav-icon" />,
+        badge: {
+          color: 'info',
+        },
+      },
+      {
+        component: CNavItem,
+        name: 'User List',
+        to: 'users',
+        icon: <CIcon icon={cilTablet} customClassName="nav-icon" />,
+        badge: {
+          color: 'info',
+        },
+      },
+    ],
+  },
 ];
 
 export default _nav_consultant;
